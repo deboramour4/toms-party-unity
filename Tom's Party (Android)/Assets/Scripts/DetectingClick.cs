5cd5c25d814e4a193e98abad609aa28bb8e9944b
AlanCesarAraujo <alancesar389@gmail.com>
2017-04-20 16:25:58 -0300

    Upgrade

---
+++ Tom's Party (Android)/Assets/Scripts/DetectingClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DetectingClick : MonoBehaviour {
+
+	public int index;
+
+	Collider2D objCollider;
+
+	// Use this for initialization
+	void Start () {
+		objCollider = GetComponent<CircleCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			if(objCollider.OverlapPoint(mousePosition)) {
+				SceneManager.LoadScene (index);
+				Debug.Log ("New Scene");
+			}
+		}
+	}
+}
