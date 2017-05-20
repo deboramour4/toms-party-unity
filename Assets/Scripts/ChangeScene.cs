71a00b7bdba32b1ce7aff551572771ba56fb050c
Débora Moura <Dbora Moura>
2017-05-20 18:17:27 -0300

    Changing every button and creating Canvas

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour {
+
+	public int index;
+
+	public void goToScene(int index){
+		SceneManager.LoadScene (index);
+		Debug.Log ("Scene"+index);
+	}
+}
