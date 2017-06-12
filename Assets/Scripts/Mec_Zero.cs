0195dbceb94917d4ebdc93490523cd5c402e66a1
aluno <aluno>
2017-06-12 17:48:10 -0300

    Movement

--- Assets/Scripts/Mec_Zero.cs
+++ Assets/Scripts/Mec_Zero.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,10 +8,13 @@ public class Mec_Zero : MonoBehaviour {
 	//adio
 	public AudioSource audioSource;
 
-	//moving
-	private int screenW;
-	private float screenSlice;
-	private int count;
+	//moviment
+	private Transform tPlayer;
+	private bool isWalking;
+	private bool isSinging;
+	private float time;
+	private float sliceOfTime;
+
 
 	//collisor
 	Collider2D objCollider;
@@ -22,26 +25,63 @@ public class Mec_Zero : MonoBehaviour {
 	void Start () {
 		objCollider = GetComponent<PolygonCollider2D> ();
 		animator = GetComponent<Animator>();
-
-		screenW = Screen.width;
-		screenSlice = (screenW - 250) / 5;
-		Debug.Log ("Largura da tela" + screenW);
-
-		Debug.Log ("teoricamente a largura da tela"+transform.position.x);
-
-
+		tPlayer = GetComponent<Transform>();
+		isWalking = false;
+		isSinging = false;
+		time = 0;
 	}
 		
 	void Update () {
 		if(Input.GetMouseButtonDown(0)) {
 			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-			if(objCollider.OverlapPoint(mousePosition) && transform.position.x>-3.5 && transform.position.x<3.5) {
-				animator.SetTrigger ("walk");
-				setX (transform.position.x+1.4f);
-				audioSource.Play();
+			if(objCollider.OverlapPoint(mousePosition)) {
+				isSinging = true;
+				sliceOfTime = time;
+				//isWalking = true;
+				Debug.Log(sliceOfTime);
+				Debug.Log(isSinging);
+
 			}
 		}
+
+		if (isSinging){
+			audioSource.Play();
+			animator.CrossFade ("sing", 0f);
+			isSinging = false;
+		}
+
+		if(isSinging && time > sliceOfTime+80)
+				isWalking = true;
+
+		if(isWalking){
+			animator.CrossFade ("walking",0f);
+			tPlayer.Translate(1.5f * Time.deltaTime,  0.0f, 0.0f );
+			//Debug.Log(1f * Time.deltaTime);
+		}
+
+		time++;
+		//Debug.Log(time);
+
+
+
+
+			//Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			//if(objCollider.OverlapPoint(mousePosition) && transform.position.x>-3.5 && transform.position.x<3.5) {
+				//animator.CrossFade ("sing",0f); //change the animation immediately
+				/*if(transform.position.x <= transform.position.x+1.4f){*/
+					
+					//tPlayer.Translate(1f * Time.deltaTime,  0.0f, 0.0f );
+
+					//animator.CrossFade ("walking",0f);
+				/*}*/
+				//setX (transform.position.x+1.4f);
+				//audioSource.Play();
+				 //animator.CrossFade ("idle", 0.3f);
+			//}
+		
+		
 	}
 
 	void setX(float n){
