22ed53f518119baf44247c594a29c8300b2b6c69
AlanCesarAraujo <alancesar389@gmail.com>
2017-07-02 20:19:42 -0300

    a few things

---
+++ Assets/Scripts/MangerScripts/ManagerScript2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScript2 : MonoBehaviour {
+
+	public GameObject objPlayer; 
+	public GameObject progressBar;
+
+	public GameObject[] objPhotos = new GameObject[3];
+	private PlayingNote[] photos = new PlayingNote[3];
+	public int[] photosID = new int[3];
+	public AudioClip[] sounds = new AudioClip[3];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
