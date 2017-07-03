be1fe25a31337971f35ecfadbe577a5995bb71c0
AlanCesarAraujo <alancesar389@gmail.com>
2017-07-03 07:20:18 -0300

    A lot of things

--- Assets/Scripts/MangerScripts/ManagerScript2.cs
+++ Assets/Scripts/MangerScripts/ManagerScript2.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class ManagerScript2 : MonoBehaviour {
 
-	public GameObject objPlayer; 
 	public GameObject progressBar;
 
 	public GameObject[] objPhotos = new GameObject[3];
-	private PlayingNote[] photos = new PlayingNote[3];
+	private PlayingNote2[] photos = new PlayingNote2[3];
 	public int[] photosID = new int[3];
 	public AudioClip[] sounds = new AudioClip[3];
 
@@ -20,5 +19,16 @@ public class ManagerScript2 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	IEnumerator Round(){
+		
+		yield return new WaitForSeconds (1f);
+		photos[0].Play ();
+		yield return new WaitForSeconds (2f);
+		photos[1].Play ();
+		yield return new WaitForSeconds (2f);
+		photos[2].Play ();
+		//isPlayable = true;
 	}
 }
