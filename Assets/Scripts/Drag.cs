f7ccb46edc0a7661ea350c507c9aef59d13ea29c
AlanCesarAraujo <alancesar389@gmail.com>
2017-07-08 18:22:38 -0300

    Still Drag and Drop

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -3,26 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Drag : MonoBehaviour {
-	Vector3 dist;
+
+	Vector3 temp;      //temporary variable to transform
+	Vector3 dist;	   
 	float posX;
 	float posY;
-	SpriteRenderer sprite;
-	SpriteRenderer other;
+	float initPosX;    //initial position of X;
+	float initPosY;    //initial position of X;
+	bool isDragging;
 
+	//Photos objects
 	public GameObject otherPhoto;
+	public GameObject staticPhoto1;
+	public GameObject staticPhoto2;
+
+	//Photos Sprites
+	SpriteRenderer sprite;
+	SpriteRenderer otherSprite;
+	SpriteRenderer staticSprite1;
+	SpriteRenderer staticSprite2;
 
 	void Start(){
+		temp = transform.position;
+		initPosX = temp.x;
+		initPosY = temp.y;
 		sprite = GetComponent<SpriteRenderer> ();
-		other = otherPhoto.GetComponent<SpriteRenderer> ();
+		otherSprite = otherPhoto.GetComponent<SpriteRenderer> ();
+		staticSprite1 = staticPhoto1.GetComponent<SpriteRenderer> ();
+		staticSprite2 = staticPhoto2.GetComponent<SpriteRenderer> ();
+		isDragging = false;
 	}
 
+	//While is dragging
 	void OnMouseDown(){
 		dist = Camera.main.WorldToScreenPoint (transform.position);
 		posX = Input.mousePosition.x - dist.x;
 		posY = Input.mousePosition.y - dist.y;
 	
 		sprite.sortingOrder = 1;
-		other.sortingOrder = 0;
+		otherSprite.sortingOrder = 0;
+		staticSprite1.sortingOrder = 1;
+		staticSprite2.sortingOrder = 0;
+
+		isDragging = true;
 	}
 
 	void OnMouseDrag(){
@@ -31,5 +54,21 @@ public class Drag : MonoBehaviour {
 		Vector3 worldPos = Camera.main.ScreenToWorldPoint (mousePos);
 		transform.position = worldPos;
 
+	}
+
+	void OnMouseUp(){
+		temp.x = initPosX;
+		temp.y = initPosY;
+		transform.position = temp;
+		isDragging = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		Debug.Log ("aeeee");
+	}
+
+
+	bool getIsDragging(){
+		return isDragging;
 	}
 }
---
+++ Assets/Scripts/Drop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour {
+
+	public GameObject portraitDo;
+	public GameObject portraitRe;
+
+	SpriteRenderer spriteDo;
+	SpriteRenderer spriteRe;
+
+	void Start(){
+		spriteDo = portraitDo.GetComponent<SpriteRenderer> ();
+		spriteRe = portraitRe.GetComponent<SpriteRenderer> ();
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		Debug.Log ("aeeee");
+		if(other.gameObject.tag == "PhotoDo"){
+			
+			portraitDo.SetActive (true);
+			spriteDo.sortingOrder = 1;
+			spriteRe.sortingOrder = 0;
+		}
+	}
+
+}
