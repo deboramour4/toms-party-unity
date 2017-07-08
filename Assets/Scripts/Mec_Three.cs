cefb41939dd3886c24ae65b3f10f9225bd41eb1a
Débora Moura <deboramoura@alu.ufc.br>
2017-07-08 03:32:35 -0300

    End of Mec Three

--- Assets/Scripts/Mec_Three.cs
+++ Assets/Scripts/Mec_Three.cs
@@ -9,6 +9,7 @@ public class Mec_Three : MonoBehaviour {
 	public GameObject objPlayer; //player
 	public GameObject[] objWood = new GameObject[4]; //woods
 	public GameObject progressBar; //progressBar
+	public GameObject victory;
 
 
 	//audios
@@ -29,10 +30,11 @@ public class Mec_Three : MonoBehaviour {
 	Animator[] woodAnimator = new Animator[4];
 	Animator pBarAnimator;
 
-	//singing
-	bool isSinging;
-	bool getTime;
-	float time;
+	//mechanic
+	public static int lastWood, actualWood;
+	public static bool firstClick;
+	public static int stars;
+	public static int totalCorrect;
 
 
 	void Start () {
@@ -50,45 +52,27 @@ public class Mec_Three : MonoBehaviour {
 		//Elements of the progress bar
 		pBarAnimator = progressBar.GetComponent<Animator>();
 
-		//animation singing
-		isSinging = false;
+		//about the mechanic
 		chooseWoodsNotes ();
+		firstClick = true;
+		stars = 0;
 	}
+				
 
-	void FixedUpdate () {
-		if (isSinging) {
-			playerAnimator.CrossFade ("sing", 0f); //change the animation immediately
-		} else {
-			playerAnimator.CrossFade ("idle", 0f);
-		}
-			
-	}
-		
 	void Update () {
 		//Make player sing
 		if(Input.GetMouseButtonDown(0)) {
 			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			if (playerCollider.OverlapPoint (mousePosition)) {
-				if (getTime) {
-					time = Time.frameCount;
-					getTime = false;
-				}
-
 				//make sing: change animation, play note
-				isSinging = true;
+				playerAnimator.SetTrigger("sing");
+				StartCoroutine ("stopSing");
 				playerAudioSource.Play ();
 			}
 		}
-						
-		//make the player stop singing after a time
-		if (isSinging && Time.frameCount > time + 170) {
-			isSinging = false;
-			getTime = true;
-		}			
-			
 	}
 
-
+	//choose random notes to the woods
 	void chooseWoodsNotes() {
 		bool valido;
 		for (int i = 0; i < 4; i++) {
@@ -104,17 +88,95 @@ public class Mec_Three : MonoBehaviour {
 		Debug.Log("note1="+woodsOrder[0]+"  note2="+woodsOrder[1]+"  note3="+woodsOrder[2]+"  note4="+woodsOrder[3]);
 		setWoodsNotes ();
 	}
-		
+
+	// set audio clips to the woods	
 	void setWoodsNotes(){
 		woodAudioSource[woodsOrder[0]].clip = sounds [0];
 		woodAudioSource[woodsOrder[1]].clip = sounds [0];
 		woodAudioSource[woodsOrder[2]].clip = sounds [1];
 		woodAudioSource[woodsOrder[3]].clip = sounds [1];
 	}
+		
 
-	public void clickWood(int wood){
-		woodAnimator [wood].SetTrigger ("show");
-		Debug.Log(woodAudioSource [wood].clip);
-		woodAudioSource [wood].Play ();
+
+	//the first wood (piece) is clicked and waits for the second click
+	IEnumerator turnFirst(){
+		//Debug.Log ("1 last : "+lastWood+" | actual : " + actualWood); 
+		yield return new WaitForSeconds (0.4f);
+		woodAudioSource [actualWood].Play ();
+		lastWood = actualWood;
+		firstClick = false;
+	}
+
+
+
+	//the second click. checks if it's the correct "par"
+	IEnumerator turnSecond(){
+		//play the note of the wood
+		yield return new WaitForSeconds (0.4f);
+		woodAudioSource [actualWood].Play ();
+
+		if (woodAudioSource [lastWood].clip == woodAudioSource [actualWood].clip) {
+			//if it's correct, mi happy, plus star, and stay normal
+			playerAnimator.SetTrigger("happy");
+			StartCoroutine ("stopHappy");
+			totalCorrect++;
+			woodAnimator [actualWood].SetTrigger ("turned");
+			woodAnimator [lastWood].SetTrigger ("turned");
+			firstClick = true;
+
+			if (totalCorrect == 2 && stars!=5) {
+				stars++;
+				pBarAnimator.SetInteger("cont", stars);
+				totalCorrect = 0;
+				StartCoroutine("newRound");
+			}
+			if (stars == 5) {
+				StartCoroutine("theEnd");
+			}
+				
+		} else {
+			//if it's wrong turn back the woods
+			playerAnimator.SetTrigger("sad");
+			StartCoroutine ("stopSad");
+			yield return new WaitForSeconds (2f);
+			woodAnimator [actualWood].SetTrigger ("hide");
+			woodAnimator [lastWood].SetTrigger ("hide");
+			firstClick = true;
+		}
+	}
+
+
+
+	//restart a new round
+	IEnumerator newRound(){
+		yield return new WaitForSeconds (3f);
+		woodAnimator [0].SetTrigger ("hide");
+		woodAnimator [1].SetTrigger ("hide");
+		woodAnimator [2].SetTrigger ("hide");
+		woodAnimator [3].SetTrigger ("hide");
+
+		chooseWoodsNotes ();
+	}
+
+	//victory screen
+	IEnumerator theEnd () {
+		yield return new WaitForSeconds (2f);
+		victory.SetActive (true);
+	}
+
+
+	//time to stop animations
+	IEnumerator stopHappy(){
+		yield return new WaitForSeconds (2.5f);
+		playerAnimator.SetTrigger("idle");
+	}
+	IEnumerator stopSad(){
+		yield return new WaitForSeconds (3f);
+		playerAnimator.SetTrigger("idle");
+	}
+	IEnumerator stopSing(){
+		yield return new WaitForSeconds (2f);
+		playerAnimator.SetTrigger("idle");
 	}
 }
---
+++ Assets/Scripts/Wood.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wood : MonoBehaviour {
+
+	public GameObject manager;
+	private Mec_Three mec = new Mec_Three ();
+	private AudioSource note;
+	private Animator animator;
+
+	void Start(){
+		note = GetComponent<AudioSource> ();
+		animator = GetComponent<Animator> ();
+		mec = manager.GetComponent<Mec_Three>();
+	}
+
+	//method called when some wood is clicked. cheks if it's the first oe second click
+	public void clickWood(int wood){
+		Mec_Three.actualWood = wood;
+		animator.SetTrigger ("show");
+
+		if (Mec_Three.firstClick) {
+			mec.StartCoroutine ("turnFirst");
+		} else {
+			mec.StartCoroutine ("turnSecond");
+		}
+	}
+
+	public void setNote(AudioClip clip){
+		this.note.clip = clip;
+	}
+
+	public AudioSource getNote(){
+		return this.note;
+	}
+
+	public void setAnimator(Animator an){
+		this.animator = an;
+	}
+
+	public Animator getAnimator(){
+		return this.animator;
+	}
+
+
+}
